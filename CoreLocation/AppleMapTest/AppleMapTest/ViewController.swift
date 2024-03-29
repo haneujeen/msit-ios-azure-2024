1523aa8b31d9fa294a60912b861f3706983b83cd
Eujeen Han <eujeenhan@gmail.com>
2024-03-29 10:06:58 +0900

    Add MapKit test

---
+++ CoreLocation/AppleMapTest/AppleMapTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMapTest
+//
+//  Created by 한유진 on 3/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
