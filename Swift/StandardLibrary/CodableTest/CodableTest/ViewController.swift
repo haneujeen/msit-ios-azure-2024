130b75adbecb6bdc5e9511a055f89501893effbe
Eujeen Han <eujeenhan@gmail.com>
2024-04-02 16:27:30 +0900

    Add Codable test

---
+++ Swift/StandardLibrary/CodableTest/CodableTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodableTest
+//
+//  Created by 한유진 on 4/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
