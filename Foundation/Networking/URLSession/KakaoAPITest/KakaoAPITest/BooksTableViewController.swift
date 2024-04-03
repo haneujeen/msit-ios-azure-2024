71a50da20e44b568840a12b63ad20d70a9bd447a
Eujeen Han <eujeenhan@gmail.com>
2024-04-03 09:30:35 +0900

    Update Kakao API test
    Add registerNib()
    Add buildRequest(query:page:size:)
    Add createTask(query:page:size)

--- Foundation/Networking/URLSession/KakaoAPITest/KakaoAPITest/BooksTableViewController.swift
+++ Foundation/Networking/URLSession/KakaoAPITest/KakaoAPITest/BooksTableViewController.swift
@@ -8,10 +8,44 @@
 import UIKit
 
 class BooksTableViewController: UITableViewController {
-
+    let apiKey = "KakaoAK ec21690271bdc78663d5780d97cae11d"
+    var books: [[String:Any]]?
+    
     func registerNib() {
         let nib = UINib(nibName: "BookNib", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "bookCell")
+        tableView.register(nib, forCellReuseIdentifier: "nib")
+    }
+    
+    func buildRequest(query: String, page: Int, size: Int) -> URLRequest {
+        let endpoint = "https://dapi.kakao.com/v3/search/book?query=\(query)&page=\(page)&size=\(size)"
+        
+        guard let endpoint = endpoint.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+              let url = URL(string: endpoint)
+        else { fatalError() }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue(apiKey, forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    func createTask(query: String, page: Int, size: Int) {
+        let session = URLSession.shared
+        let task = session.dataTask(with: buildRequest(query: query, page: page, size: size)) { data, response, error in
+            if let error { print(error.localizedDescription); return }
+            
+            guard let data else { return }
+            guard let object = try? JSONSerialization.jsonObject(with: data) as? [String:Any] else { return }
+            
+            self.books = object["documents"] as? [[String:Any]]
+            
+            guard let meta = object["meta"] as? [String:Any], let isEnd = meta["is_end"] as? Bool else { return }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
     }
     
     override func viewDidLoad() {
@@ -22,6 +56,7 @@ class BooksTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         registerNib()
+        createTask(query: "해리포터", page: 1, size: 1)
     }
 
     
@@ -29,23 +64,30 @@ class BooksTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return books?.count ?? 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nib", for: indexPath) as! BookNib
 
         // Configure the cell...
+        guard let book = books?[indexPath.row] else { return cell }
+        
+        cell.headLabel.text = book["title"] as? String
+        
+        let authors = book["authors"] as? [String]
+        cell.subheadLabel.text = authors?.joined(separator: ", ")
+        
+        guard let price = book["price"] as? Int else { return cell}
+        cell.priceLabel.text = "\(price)"
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
