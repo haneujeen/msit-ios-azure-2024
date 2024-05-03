71b03541189a85b6846f4007576853a9ac9f93fe
Eujeen Han <eujeenhan@gmail.com>
2024-05-03 17:46:56 +0900

    Create middleware verify.js

---
+++ XServer/routes/verify.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const secret = process.env.JWT_SECRET
+
+const isVerified = async (req, res, next) => {
+    const auth = req.get('Authorization')
+    if (!(auth && auth.startsWith('Bearer '))) {
+        res.json({ success: false, message: "No token" })
+    }
+
+    const token = auth.split(' ')[1]
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            res.json({ success: false, message: "Invalid token" })
+        } else {
+            req.userId = decoded.uid
+            req.role = decoded.rol
+            next()
+        }
+    })
+}
+
+module.exports = isVerified
