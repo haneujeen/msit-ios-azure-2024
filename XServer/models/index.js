8859e47df26798da765f9b8d2553b7817be9d4b8
Eujeen Han <eujeenhan@gmail.com>
2024-05-03 14:18:23 +0900

    Add User model

--- XServer/models/index.js
+++ XServer/models/index.js
@@ -4,6 +4,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('/../config/config.json')[env];
 const db = {};
 
+let sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+)
+
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
