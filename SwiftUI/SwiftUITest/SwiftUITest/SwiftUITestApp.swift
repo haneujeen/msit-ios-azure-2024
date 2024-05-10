7808eb44fdb2572b8cdfbd4b2102629c93026e54
Eujeen Han <eujeenhan@gmail.com>
2024-05-10 09:34:20 +0900

    Create SwiftUI test

---
+++ SwiftUI/SwiftUITest/SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by 한유진 on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
