493353250b5058437298b7623a5a7a3a8c1874ad
Eujeen Han <eujeenhan@gmail.com>
2024-05-10 11:50:08 +0900

    Create Landmarks

---
+++ SwiftUI/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 한유진 on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
