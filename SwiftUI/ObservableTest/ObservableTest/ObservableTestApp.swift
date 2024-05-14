d542a23dac725cbed98fc0ecbeb7ed3d5977847d
Eujeen Han <eujeenhan@gmail.com>
2024-05-14 17:09:41 +0900

    Create Observable test

---
+++ SwiftUI/ObservableTest/ObservableTest/ObservableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableTestApp.swift
+//  ObservableTest
+//
+//  Created by 한유진 on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
