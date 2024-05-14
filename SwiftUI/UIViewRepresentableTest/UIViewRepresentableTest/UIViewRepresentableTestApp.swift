36d8533d8cd948c6d45aef59624c2fc6a920f9e8
Eujeen Han <eujeenhan@gmail.com>
2024-05-14 11:29:24 +0900

    Create UIViewRepresentableTest

---
+++ SwiftUI/UIViewRepresentableTest/UIViewRepresentableTest/UIViewRepresentableTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewRepresentableTestApp.swift
+//  UIViewRepresentableTest
+//
+//  Created by 한유진 on 5/14/24.
+//
+
+import SwiftUI
+
+@main
+struct UIViewRepresentableTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
